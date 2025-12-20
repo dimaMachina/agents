de1c5625e9772614780299b984b8d5d4365627f7
Dima <dima@inkeep.com>
2025-12-20 12:43:10 +0700

    tenant layout

--- agents-manage-ui/src/app/[tenantId]/layout.tsx
+++ agents-manage-ui/src/app/[tenantId]/layout.tsx
@@ -1,10 +1,45 @@
-import { AppSidebarProvider } from '@/components/sidebar-nav/app-sidebar-provider';
-import { SidebarInset } from '@/components/ui/sidebar';
+'use client';
 
-export default function Layout({ children }: LayoutProps<'/[tenantId]'>) {
+import { AppSidebarProvider } from '@/components/sidebar-nav/app-sidebar-provider';
+import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar';
+import { cn } from '@/lib/utils';
+import { Separator } from '@/components/ui/separator';
+import { HeaderMenus } from '@/components/layout/header-menus';
+
+export default function Layout({ children, breadcrumbs }: LayoutProps<'/[tenantId]'>) {
   return (
     <AppSidebarProvider>
-      <SidebarInset>{children}</SidebarInset>
+      <SidebarInset>
+        <div className="h-[calc(100vh-16px)] flex flex-col overflow-hidden">
+          <header className="h-(--header-height) shrink-0 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) bg-muted/20 dark:bg-background rounded-t-[14px] flex items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1 text-muted-foreground hover:text-foreground hover:bg-accent dark:text-muted-foreground dark:hover:text-foreground dark:hover:bg-accent/50" />
+            <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
+            {breadcrumbs}
+            <Separator
+              orientation="vertical"
+              className="mx-2 data-[orientation=vertical]:h-4 ml-auto"
+            />
+            <HeaderMenus />
+          </header>
+          <div
+            className={cn(
+              'flex flex-col flex-1 overflow-y-auto scrollbar-thin',
+              'scrollbar-thumb-muted-foreground/30 dark:scrollbar-thumb-muted-foreground/50 scrollbar-track-transparent h-full w-full min-h-0 bg-muted/20 dark:bg-background'
+            )}
+          >
+            <div
+              id="main-content"
+              className={cn(
+                '@container', // do not overflow the container
+                'w-full p-6 grow'
+                // className
+              )}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
     </AppSidebarProvider>
   );
 }
