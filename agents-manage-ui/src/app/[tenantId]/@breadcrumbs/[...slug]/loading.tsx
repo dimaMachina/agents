e8d4945d5e0d782475013bcaf7bba821d14ed339
Dima <dima@inkeep.com>
2025-12-29 14:40:43 +0700

    improve skeleton [skip ci]

--- agents-manage-ui/src/app/[tenantId]/@breadcrumbs/[...slug]/loading.tsx
+++ agents-manage-ui/src/app/[tenantId]/@breadcrumbs/[...slug]/loading.tsx
@@ -2,7 +2,13 @@ import type { FC } from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
 
 const Loading: FC = () => {
-  return <Skeleton className="h-5 w-14" />;
+  return (
+    <>
+      <Skeleton className="h-5 w-14" />
+      <div className='after:content-["/"] after:text-muted-foreground/60' />
+      <Skeleton className="h-5 w-14" />
+    </>
+  );
 };
 
 export default Loading;
